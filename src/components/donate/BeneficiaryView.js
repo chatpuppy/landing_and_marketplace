9cdf6f15daad62b2fcacdbe7ffbd122a355b111d
codingsh <codingsh@pm.me>
2022-02-27 19:53:52 +0000

    beneficiary page: add realease and redeem actions

--- src/components/donate/BeneficiaryView.js
+++ src/components/donate/BeneficiaryView.js
@@ -1,13 +1,107 @@
-import { Stack, Heading, SimpleGrid, Text, Button, Box , useBreakpointValue} from '@chakra-ui/react'
+import { Stack, Heading, SimpleGrid, Text, Button, Box , useBreakpointValue,  useToast,} from '@chakra-ui/react'
 import { Card } from '../common/Card';
+import { useAuth } from "contexts/AuthContext";
 import { useDonate } from "contexts/DonateContext";
 import { getNameSaleById } from "utils/getNameSaleById";
 import PriceRangeComponent  from "./PriceRangeComponent";
 
 import { ethers } from "ethers";
+import BeneficiaryModal from './beneficiaryModal';
 
 export const BeneficiaryView = () => {
     const {participantID, totalAmount, beneficiaryCount, priceRange} = useDonate()
+    const [ isLoading, setIsLoading ] = useState(false);
+    const { currentAccount, currentNetwork, tokenVestingContract } = useAuth()
+    const toast = useToast()
+    const id = 'toast'
+
+    let uint8 = new Uint8Array(2);
+    uint8[0] = participantID;
+    let participant = uint8[0];
+
+
+    const actionRedeem = async() => {
+      if (!currentAccount && !currentNetwork === 42) return 0
+      setIsLoading(true);
+      try {
+        await tokenVestingContract.redeem(participant)
+        toast({
+          title: 'Redeem',
+          description: `Redeem now`,
+          status: 'sucess',
+          duration: 4000,
+          isClosable: true,
+      })
+      setTimeout(()=>{
+          window.location.reload();
+      }, 5000)
+      } catch(err) {
+        console.log(err)
+        setIsLoading(false);
+      }
+    }
+
+    const actionRelease = async() => {
+      if (!currentAccount && !currentNetwork === 42) return 0
+      setIsLoading(true);
+      try {
+        await tokenVestingContract.release(participant)
+        toast({
+          title: 'Release',
+          description: `Release now`,
+          status: 'sucess',
+          duration: 4000,
+          isClosable: true,
+      })
+      setTimeout(()=>{
+          window.location.reload();
+      }, 5000)
+      } catch(err) {
+        console.log(err)
+        setIsLoading(false);
+      }
+    }
+
+    const DonatedButtons = () => (
+      <Box
+      justifyContent={"center"}
+      textAlign={"center"}
+      as={"form"}
+      mt={10}
+      mr={10}
+      borderRight={20}
+    >
+      <Button
+        fontFamily={"heading"}
+        mt={8}
+        w={"full"}
+        bgGradient="linear(to-r, red.400,pink.400)"
+        color={"white"}
+        _hover={{
+          bgGradient: "linear(to-r, red.400,pink.400)",
+          boxShadow: "xl",
+        }}
+        onClick={actionRelease}
+      >
+        Release
+      </Button>
+      <Button
+        fontFamily={"heading"}
+        mt={8}
+        w={"full"}
+        bgGradient="linear(to-r, red.400,pink.400)"
+        color={"white"}
+        _hover={{
+          bgGradient: "linear(to-r, red.400,pink.400)",
+          boxShadow: "xl",
+        }}
+        onClick={actionRedeem}
+      >
+        Redeem
+      </Button>
+    </Box>
+    )
+
     return (
         <Stack
           spacing={{
@@ -85,42 +179,3 @@ export const BeneficiaryView = () => {
       )
 }
 
-const DonatedButtons = () => (
-    <Box
-    justifyContent={"center"}
-    textAlign={"center"}
-    as={"form"}
-    mt={10}
-    mr={10}
-    borderRight={20}
-  >
-    <Button
-      fontFamily={"heading"}
-      mt={8}
-      w={"full"}
-      bgGradient="linear(to-r, red.400,pink.400)"
-      color={"white"}
-      _hover={{
-        bgGradient: "linear(to-r, red.400,pink.400)",
-        boxShadow: "xl",
-      }}
-      onClick={() => console.log('release')}
-    >
-      Release
-    </Button>
-    <Button
-      fontFamily={"heading"}
-      mt={8}
-      w={"full"}
-      bgGradient="linear(to-r, red.400,pink.400)"
-      color={"white"}
-      _hover={{
-        bgGradient: "linear(to-r, red.400,pink.400)",
-        boxShadow: "xl",
-      }}
-      onClick={() => console.log('Reedeem')}
-    >
-      Reedeem
-    </Button>
-  </Box>
-  )
