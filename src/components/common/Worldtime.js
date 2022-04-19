d36805ee41f82a65c701335f37a26b5ab5550561
jackygu2006 <754800643@qq.com>
2022-04-19 16:36:13 +0800

    Fixed some bugs of donation

--- src/components/common/Worldtime.js
+++ src/components/common/Worldtime.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 
 export const getGlobalTime = async() => {
-	const url = "http://worldtimeapi.org/api/timezone/Etc/UTC";
+	const url = "https://worldtimeapi.org/api/timezone/Etc/UTC";
 	const response = await axios.get(url);
 	if(response.status !== 200) return 0;
 	else return response.data.unixtime;
