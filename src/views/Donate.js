a3283b7b1502f24e265b2979f752acf5a37e15df
jackygu2006 <754800643@qq.com>
2022-04-20 09:18:02 +0800

    Fixed some bugs of donation

--- src/views/Donate.js
+++ src/views/Donate.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from 'components/Footer'
-import AddressFooter from 'components/AddressFooter';
+// import AddressFooter from 'components/AddressFooter';
 import NavBar from 'components/NavBar'
 import DonateComponent from 'components/donate/DonateComponent'
 
@@ -9,9 +9,9 @@ export default function Donate() {
     <>
       <NavBar />
       <DonateComponent />
-			<AddressFooter
+			{/* <AddressFooter
         chainId={97}
-      />
+      /> */}
       <Footer />
     </>
   )
