65f0b4f1664f5e6806b2ff75340fb5c62d56b774
chatpuppy <ydfuf1dn@gmail.com>
2022-12-29 13:55:39 +0800

    update privacy policy

--- src/views/Privacy.js
+++ src/views/Privacy.js
@@ -12,33 +12,16 @@ export default function Donate() {
 <p>By using the Services, you accept the terms of this Policy and our Terms of Use, and consent to our collection, use, disclosure, and retention of your information as described in this Policy.  If you have not done so already, please also review our terms of use. The terms of use contain provisions that limit our liability to you and require you to resolve any dispute with us on an individual basis and not as part of any class or representative action. IF YOU DO NOT AGREE WITH ANY PART OF THIS PRIVACY POLICY OR OUR TERMS OF USE, THEN PLEASE DO NOT USE ANY OF THE SERVICES.</p>
 <p><strong>WHAT WE COLLECT</strong></p>
 <p>We get information about you in a range of ways. </p>
-<p><strong>Information You Give Us.</strong> Information we collect from you may include:</p>
-<ul>
-<li>Technical information, such as your Ethereum wallet address, public key, application programming interface (API)-key and network information regarding transactions.</li>
-</ul>
-<p><strong>Information we will never collect</strong>. We will never ask you to share your private keys or wallet seed. Never trust anyone or any site that asks you to enter your private keys or wallet seed.</p>
-<p><strong>USE OF PERSONAL INFORMATION</strong></p>
-<p><strong><em>To provide our service</em></strong></p>
-<p>We will use your personal information in the following ways:</p>
-<ul>
-<li>To enable you to access and use the Services</li>
-<li>To provide and deliver products and services that you may request.</li>
-</ul>
-<p><strong><em>To comply with law</em></strong></p>
-<p>We use your personal information as we believe necessary or appropriate to comply with applicable laws (including anti-money laundering (AML) laws and know-your-customer (KYC) requirements), lawful requests and legal process, such as to respond to subpoenas or requests from government authorities.  </p>
-<p><strong><em>To optimize our platform</em></strong></p>
-<p>In order to optimize your user experience, we may use your personal information to operate, maintain, and improve our Services. We may also use your information to respond to your comments and questions regarding the Services, and to provide you and other users with general customer service. </p>
-<p><strong><em>For compliance, fraud prevention, and safety</em></strong></p>
-<p>We may use your personal information to protect, investigate, and deter against fraudulent, unauthorized, or illegal activity.</p>
-<p><strong>SHARING OF PERSONAL INFORMATION</strong></p>
-<p>We do not share the personal information that you provide us with other organizations.</p>
+<p><strong>We will not collect any information from you</strong>. We will never ask you to share your personal information and private keys or wallet seed. Never trust anyone or any site that asks you to enter your private keys or wallet seed.</p>
+<p><strong>USE AND SHARING OF PERSONAL INFORMATION</strong></p>
+<p>We do not use or share the personal information with any organizations.</p>
 <p><strong>HOW INFORMATION IS SECURED</strong> </p>
 <p>We retain information we collect as long as it is necessary and relevant to fulfill the purposes outlined in this privacy policy. In addition, we retain personal information to comply with applicable law where required, prevent fraud, resolve disputes, troubleshoot problems, assist with any investigation, enforce our Terms of Use, and other actions permitted by law. To determine the appropriate retention period for personal information, we consider the amount, nature, and sensitivity of the personal information, the potential risk of harm from unauthorized use or disclosure of your personal information, the purposes for which we process your personal information and whether we can achieve those purposes through other means, and the applicable legal requirements.</p>
 <p>In some circumstances we may anonymize your personal information (so that it can no longer be associated with you) in which case we may use this information indefinitely without further notice to you.</p>
 <p>We employ industry standard security measures designed to protect the security of all information submitted through the Services. However, the security of information transmitted through the internet can never be guaranteed. We are not responsible for any interception or interruption of any communications through the internet or for changes to or losses of data. Users of the Services are responsible for maintaining the security of any password, biometrics, user ID or other form of authentication involved in obtaining access to password protected or secure areas of any of our digital services. In order to protect you and your data, we may suspend your use of any of the Services, without notice, pending an investigation, if any breach of security is suspected.</p>
 <p><strong>ELIGIBILITY</strong></p>
 <p>If you are under the age of majority in your jurisdiction of residence, you may use the Services only with the consent of or under the supervision of your parent or legal guardian. Consistent with the requirements of the Children’s Online Privacy Protection Act (COPPA), if we learn that we have received any information directly from a child under age 13 without first receiving his or her parent’s verified consent, we will use that information only to respond directly to that child (or his or her parent or legal guardian) to inform the child that he or she cannot use the Sites and subsequently we will delete that information.</p>
-<p><strong>MARKETING COMMUNICATIONS AND SHARING</strong></p>
+<p><strong>MARKETING AND MOBILE COMMUNICATIONS AND SHARING</strong></p>
 <p>if you have downloaded our mobile application and enabled push notifications on your mobile device, we may send you alerts and notifications through push notifications, for example, to communicate status updates on our Services. However, you may choose to disable these notifications (except for the initial notification intended to verify your identity).</p>
 <p><strong>THIRD PARTY LINKS AND WEBSITES</strong></p>
 <p>This Privacy Notice does not address, and we are not responsible for, the privacy practices of any third parties, including those that operate websites to which our Sites link. The inclusion of a link on our Sites does not imply that we or our affiliates endorse the practices of the linked website. </p>
